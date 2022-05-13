de9d5b0a06d03b38590469d50807dded6a20cf33
Polya404 <polyapolya404@gmail.com>
2022-05-13 22:48:51 +0300

    Initial commit

---
+++ src/main/java/Distance.java
@@ -0,0 +1,12 @@
+public class Distance {
+    private double distance;
+
+    public static void print(){
+
+    }
+    static class Converter{
+        static double convert(double distance){
+            return distance;
+        }
+    }
+}
