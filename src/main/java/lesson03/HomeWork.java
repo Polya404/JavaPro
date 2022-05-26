22b9cfaffe07fae588512ddde739b8f7ab89635c
Polya404 <polyapolya404@gmail.com>
2022-05-26 23:02:34 +0300

    Initial commit

--- src/main/java/lesson03/HomeWork.java
+++ src/main/java/lesson03/HomeWork.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class HomeWork {
+    /**
+     * При помощи цикла for вывести на экран нечетные числа от 1 до 99.
+     */
     public static void numbers() {
         for (int i = 0; i < 100; i++) {
             System.out.println(i);
@@ -11,6 +14,9 @@ public class HomeWork {
     }
 
     public static void factorial(int n) {
+        /**
+         * Дано число n при помощи цикла for посчитать факториал n!
+         */
         int res = 1;
         for (int i = 1; i <= n; i++) {
             res *= i;
@@ -19,6 +25,9 @@ public class HomeWork {
     }
 
     public static void task12while(int n) {
+        /**
+         * Перепишите программы (1 и 2) с использованием цикла while.
+         */
         int f = n;
         int number = 0;
         int res = 1;
@@ -36,6 +45,9 @@ public class HomeWork {
     }
 
     public static void task12doWhile(int n) {
+        /**
+         * Перепишите программы (1 и 2) с использованием цикла do - while.
+         */
         int f = n;
         int number = 0;
         int res = 1;
@@ -53,6 +65,9 @@ public class HomeWork {
     }
 
     public static void pow(int x, int n) {
+        /**
+         * Даны переменные x и n вычислить x^n.
+         */
         int res = 1;
         for (int i = 1; i <= n; i++) {
             res *= x;
@@ -61,12 +76,18 @@ public class HomeWork {
     }
 
     public static void subsequence() {
+        /**
+         * Вывести 10 первых чисел последовательности 0, -5,-10,-15..
+         */
         for (int i = 0; i < 50; i += 5) {
             System.out.println(i * -1);
         }
     }
 
     public static void mult(int x) {
+        /**
+         * Необходимо вывести на экран таблицу умножения на Х (х любое число в диапазоне 0 .. 10)
+         */
         for (int i = 0; i < 10; i++) {
             System.out.print(i * x + "\t");
         }
@@ -74,6 +95,10 @@ public class HomeWork {
     }
 
     public static void arr() {
+        /**
+         * Создайте массив, содержащий 10 первых нечетных чисел.
+         * Выведете элементы массива на консоль в одну строку, разделяя запятой.
+         */
         int[] arr = new int[10];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = 2 * i + 1;
@@ -86,6 +111,11 @@ public class HomeWork {
     }
 
     public static int arrayMin(int[] arr) {
+        /**
+         * Дан массив размерности N, найти наименьший элемент
+         * массива и вывести на консоль (массив заполнить
+         * случайными числами из диапазона 0 .. 100)
+         */
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (Math.random() * 101);
         }
@@ -100,6 +130,9 @@ public class HomeWork {
     }
 
     public static int arrayMax(int[] arr) {
+        /**
+         * В массиве из задания 9. найти наибольший элемент
+         */
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (Math.random() * 101);
         }
@@ -114,6 +147,11 @@ public class HomeWork {
     }
 
     public static void replace(int[] arr) {
+        /**
+         * Поменять наибольший и наименьший элементы массива местами.
+         * Пример: дан массив {4, -5, 0, 6, 8}.
+         * После замены будет выглядеть {4, 8, 0, 6, -5}.
+         */
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (Math.random() * 101);
         }
@@ -132,6 +170,10 @@ public class HomeWork {
     }
 
     public static void chess() {
+        /**
+         * Вывести на экран шахматную доску 8х8 в
+         * виде 2 мерного массива (W - белые клетки , B - черные клети)
+         */
         String[][] chessBoard = new String[8][8];
         for (int i = 0; i < chessBoard.length; i++) {
             for (int j = 0; j < chessBoard[0].length; j++) {
--- src/main/java/lesson03/Main.java
+++ src/main/java/lesson03/Main.java
@@ -5,17 +5,17 @@ import static lesson03.HomeWork.*;
 public class Main {
     public static void main(String[] args) {
         numbers();
-        factorial(6);
-        task12while(5);
-        task12doWhile(4);
-        pow(3, 3);
-        subsequence();
-        mult(2);
-        arr();
+//        factorial(6);
+//        task12while(5);
+//        task12doWhile(4);
+//        pow(3, 3);
+//        subsequence();
+//        mult(2);
+//        arr();
         int[] arr = new int[6];
-        System.out.println(arrayMin(arr));
-        System.out.println(arrayMax(arr));
-               replace(arr);
-        chess();
+//        System.out.println(arrayMin(arr));
+//        System.out.println(arrayMax(arr));
+//        replace(arr);
+//        chess();
     }
 }
