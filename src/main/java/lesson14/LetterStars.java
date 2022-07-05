edca1dab036979c11b0df15d2828ac5d25362c5d
Polya404 <polyapolya404@gmail.com>
2022-07-05 10:02:16 +0300

    Initial commit

--- src/main/java/lesson14/LetterStars.java
+++ src/main/java/lesson14/LetterStars.java
@@ -11,7 +11,7 @@ public class LetterStars {
 
     static void printPhraz(char[][][] arr) {
         int count = 0;
-        char[][] pole = new char[arr[0].length][arr.length * arr[0][0].length + 4];;
+        char[][] pole = new char[arr[0].length][arr.length * arr[0][0].length + arr[0].length];;
         for (int j = 0; j < arr.length; j ++){
             for (int i = 0; i < arr[0].length; i++) {
                 fillArrFromOther(pole[i], count, arr[0][i].length + count, arr[count][i]);
@@ -21,10 +21,10 @@ public class LetterStars {
                 fillArrFromOther(pole[i], count * arr[0][i].length + count, 3 * arr[0][i].length + count, arr[count][i]);
                 count++;
                 fillArrFromOther(pole[i], count * arr[0][i].length + count, 4 * arr[0][i].length + count, arr[count][i]);
-//                count++;
-//                fillArrFromOther(pole[i], count * arr[0][i].length + count, 5 * arr[0][i].length + count, arr[count][i]);
-//                count++;
-//                fillArrFromOther(pole[i], count * arr[0][i].length + count, 6 * arr[0][i].length + count, arr[count][i]);
+                count++;
+                fillArrFromOther(pole[i], count * arr[0][i].length + count, 5 * arr[0][i].length + count, arr[count][i]);
+                count++;
+                fillArrFromOther(pole[i], count * arr[0][i].length + count, 6 * arr[0][i].length + count, arr[count][i]);
                 count = 0;
             }
         }
--- src/main/java/lesson14/NumberPrint.java
+++ src/main/java/lesson14/NumberPrint.java
@@ -1,5 +1,6 @@
 package lesson14;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,36 +8,51 @@ import static lesson14.LetterStars.printPhraz;
 
 public class NumberPrint {
     public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
         HashMap<String, char[][]> nums = new HashMap<>();
+        if (args.length == 0) {
+            System.out.println("Enter numbers");
+        }
         for (String arg : args) {
             switch (arg) {
                 case "0":
+                    arrayList.add("0");
+                    nums.put(arg, Numbers.zero);
                     break;
                 case "1":
+                    arrayList.add("1");
                     nums.put(arg, Numbers.one);
                     break;
                 case "2":
+                    arrayList.add("2");
                     nums.put(arg, Numbers.two);
                     break;
                 case "3":
+                    arrayList.add("3");
                     nums.put(arg, Numbers.three);
                     break;
                 case "4":
+                    arrayList.add("4");
                     nums.put(arg, Numbers.four);
                     break;
                 case "5":
+                    arrayList.add("5");
                     nums.put(arg, Numbers.five);
                     break;
                 case "6":
+                    arrayList.add("6");
                     nums.put(arg, Numbers.six);
                     break;
                 case "7":
+                    arrayList.add("7");
                     nums.put(arg, Numbers.seven);
                     break;
                 case "8":
+                    arrayList.add("8");
                     nums.put(arg, Numbers.eight);
                     break;
                 case "9":
+                    arrayList.add("9");
                     nums.put(arg, Numbers.nine);
                     break;
                 default:
@@ -45,11 +61,42 @@ public class NumberPrint {
 
         }
         char[][][] res = new char[args.length][][];
-        int count = 0;
-        for (Map.Entry<String, char[][]> pair : nums.entrySet()) {
-            char[][] value = pair.getValue();
-            res[count] = value;
-            count++;
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            switch (arrayList.get(i)) {
+                case "0":
+                    res[i] = nums.get("0");
+                    break;
+                case "1":
+                    res[i] = nums.get("1");
+                    break;
+                case "2":
+                    res[i] = nums.get("2");
+                    break;
+                case "3":
+                    res[i] = nums.get("3");
+                    break;
+                case "4":
+                    res[i] = nums.get("4");
+                    break;
+                case "5":
+                    res[i] = nums.get("5");
+                    break;
+                case "6":
+                    res[i] = nums.get("6");
+                    break;
+                case "7":
+                    res[i] = nums.get("7");
+                    break;
+                case "8":
+                    res[i] = nums.get("8");
+                    break;
+                case "9":
+                    res[i] = nums.get("9");
+                    break;
+                default:
+                    System.out.println("Enter only numbers");
+            }
         }
         printPhraz(res);
     }
